5ffd3421b99b72769b09cacb9a4d290bffbc2b7f
bhavesh1780 <bhavesh1780@gmail.com>
2017-05-18 14:38:48 +0100

    new Updated

--- src/test/java/com/LoadProperty/BaseTest.java
+++ src/test/java/com/LoadProperty/BaseTest.java
@@ -2,7 +2,6 @@ package com.LoadProperty;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,7 +13,7 @@ public class BaseTest extends BasePage
     {
     BrowserSelector browserSelector = new BrowserSelector();
 
-   @Parameters({"browser"})
+//   @Parameters({"browser"})
    @BeforeMethod
     public void openBrowser(String browser){
        browserSelector.selectBrowser(browser);
